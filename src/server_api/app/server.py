73326e1bce9ce417615e23aa1442c62aa9cec264
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2022-11-15 16:01:17 +0300

    :shirt: mini refactoring

--- src/server_api/app/server.py
+++ src/server_api/app/server.py
@@ -2,23 +2,31 @@ from typing import Union
 from fastapi import FastAPI
 import json
 import collections
-
-# TODO: refactoring
 import psycopg2
 
 app = FastAPI()
 
 
+class PostgreSQL:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            database="billing",
+            user="test_user",
+            password="123",
+            host="postgres_db",
+            port="5432"
+        )
+        self.conn.autocommit = True
+
+    def connect(self):
+        return self.conn
+
+
 @app.get("/")
 def read_all_customers():
     # queue connect
-    con = psycopg2.connect(
-        database="billing",
-        user="test_user",
-        password="123",
-        host="postgres_db",
-        port="5432"
-    )
+    postgres = PostgreSQL()
+    con = postgres.connect()
     cur = con.cursor()
     cur.execute("select * from customer;")
     sel = cur.fetchall()
